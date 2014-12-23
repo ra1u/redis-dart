993b94a6276d27ee4e5deebd2e3cf45057b64bba
Luka Rahne <luka.rahne@gmail.com>
2014-12-23 21:23:50 +0100

    transaction return null on error an "OK" on sucess

--- lib/transaction.dart
+++ lib/transaction.dart
@@ -15,28 +15,35 @@ class _WarrningConnection {
 }
 
 class Transaction extends Command{ 
-    Queue<Completer> _queue = new Queue();
-    RedisConnection _swaped_conn = null;
-    Command _overrided_command;
-    
-    Transaction(Command command):super(command._connection){
-      _overrided_command = command;
-      //we override his _connection, during transaction (finding bugis easier) 
-      command._connection = new _WarrningConnection();
-    }
-    
-    Future _send(object){
-      Completer c= new Completer();
-      _queue.add(c);
-      super._send(object).then((_){}); //todo handle error
-      return c.future;
-    }
-    
-    Future exec(){
-      _overrided_command._connection = this._connection;
-      Completer completer= new Completer();
-      super._send(["EXEC"])
-      .then((list){
+  Queue<Completer> _queue = new Queue();
+  RedisConnection _swaped_conn = null;
+  Command _overrided_command;
+  
+  Transaction(Command command):super(command._connection){
+    _overrided_command = command;
+    //we override his _connection, during transaction (finding bugis easier) 
+    command._connection = new _WarrningConnection();
+  }
+  
+  Future _send(object){
+    Completer c= new Completer();
+    _queue.add(c);
+    super._send(object).then((_){}); //todo handle error
+    return c.future;
+  }
+  
+  Future exec(){
+    _overrided_command._connection = this._connection;
+    return super._send(["EXEC"])
+    .then((list){
+      if(list == null){ //we got explicit error from redis
+        while(_queue.isNotEmpty){
+          Completer c =_queue.removeFirst();
+          c.complete(new RedisError("transation terminated"));
+          return null;
+        }
+      }
+      else{
         if(list.length != _queue.length){
           int diff = list.length - _queue.length;
           throw "There was $diff command(s) executed during transcation,"
@@ -46,9 +53,9 @@ class Transaction extends Command{
         for(int i=0;i<len;++i){
           Completer c =_queue.removeFirst();
           c.complete(list[i]);
-       }
-        completer.complete(null);
-      });
-      return completer.future;
-    }
+        }
+        return "OK";
+      }
+    });
+  }
 }
