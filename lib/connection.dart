890538fdc50516e98ee088df306108b7f06d2c55
Luka Rahne <luka.rahne@gmail.com>
2014-12-28 00:54:06 +0100

    connection move public to private

--- lib/connection.dart
+++ lib/connection.dart
@@ -10,12 +10,14 @@
 
 part of redis;
 
+/// Class for server connection on server
 class RedisConnection{
   Socket _socket = null;
   LazyStream _stream = null;
   Future _future = new Future.value();
   
-  Future connect(host, port){
+  /// connect on Redis server as client
+  Future<Command> connect(host, port){
     return Socket.connect(host, port)
     .then((Socket sock){
       _socket = sock;
@@ -32,7 +34,7 @@ class RedisConnection{
   //this doesnt send anything
   //it just wait something to come from socket
   //it parse it and execute future
-  Future senddummy(){
+  Future _senddummy(){
     Completer completer = new Completer.sync();
     _future = _future.then((_) =>
         RedisParser.parseredisresponse(_stream)
@@ -41,14 +43,25 @@ class RedisConnection{
     return completer.future;
   }
   
-  Future sendraw(List data){
-    _socket.add(data);
-    return senddummy();
+  // return future that complets
+  // when all prevous futures finished
+  Future _getdummy(){
+    Completer completer = new Completer.sync();
+    _future = _future.then((_){
+        completer.complete("dummy data");
+      }
+    );
+    return completer.future;
   }
   
-  Future send(object){
+  Future _sendraw(List data){
+    _socket.add(data);
+    return _senddummy();
+  }
+  
+  Future _send(object){
     var s = RedisSerialise.Serialise(object);
-    return sendraw(s);
+    return _sendraw(s);
   }
   
   void disable_nagle(bool v){
