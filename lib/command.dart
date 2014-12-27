b0f06adea580e90c5edf7684b041f8cbd6426908
Luka Rahne <luka.rahne@gmail.com>
2014-12-28 00:52:52 +0100

    document comments

--- lib/command.dart
+++ lib/command.dart
@@ -15,20 +15,35 @@ class Command {
 
   Command(this._connection){}
   
-  Future _send(Object ls) => _connection.sendraw(RedisSerialise.Serialise(ls));
+  Future _send(Object ls) => _connection._sendraw(RedisSerialise.Serialise(ls));
   
-  //proxy send
+  /// Serialise and send data to server  
+  /// 
+  /// Data can be any object recognised by Redis
+  /// List, integer, Bulk, null 
+  /// Redis command is List<String>
+  /// example SET
+  /// 
+  ///     send_object(["SET","key","value"]);
   Future send_object(Object v) => _send(v);
   
+  /// return future that completes when
+  /// all prevous packets are processed
+  Future send_nothing() => _connection._getdummy();
+  
+  /// Set socket settings for sending transations
+  /// 
+  ///  This is optimisation and not requrement. 
   void pipe_start() => _connection.disable_nagle(false); //we want to use sockets buffering
+  /// Requred to be called after last piping command
   void pipe_end() =>   _connection.disable_nagle(true);
   
   //commands in future, we will add more commands
   Future set(String key, String value) => _send(["SET",key,value]);
   Future get(String key) => _send(["GET",key]);
   
-  //transations
-  Future multi(){ //multi retun transation as future
+  /// Transations are started with multi and completed with exec()
+  Future<Transaction> multi(){ //multi retun transation as future
     return _send(["MULTI"]).then((_) => new Transaction(this));
   }
   
