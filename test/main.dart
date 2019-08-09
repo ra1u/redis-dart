950d9b97f74f1e54a84b2b238c849cc46fba21b2
soenke <me@eknoes.de>
2019-08-10 01:17:34 +0200

    Implement tests using Darts test framework

--- test/main.dart
+++ test/main.dart
@@ -7,76 +7,16 @@
  * Luka Rahne
  */
 
-library testredis;
 import 'dart:async';
-import 'dart:collection';
-import 'dart:convert';
-import 'dart:io';
-import '../lib/redis.dart';
 
-part 'testcas.dart';
-part 'testtransaction.dart';
-part 'testperformance.dart';
-part 'testpubsub.dart';
-part 'testlua.dart';
-part 'testunicode.dart';
-part 'testerror.dart';
-part 'testconversion.dart';
+import 'package:redis/redis.dart';
+import 'package:test/test.dart';
 
-
-Future testing_performance(Function fun,String name, int rep){
-   Future r = new Future((){
-       print("  starting performance test $name with $rep");
-       int start = new DateTime.now().millisecondsSinceEpoch;
-       return fun(rep).then((_){
-         int now = new DateTime.now().millisecondsSinceEpoch;
-         double diff = (now - start)/1000.0;
-         double perf = rep/diff;
-         print("  $name performance test complete , performance ${perf.round()} ops/s");
-       });
-   });
-   return testing_helper(r,name);
+Future<Command> generate_connect() {
+  return RedisConnection().connect('localhost', 6379);
 }
 
-Future testing_helper(Future f,String name){
-  print("start  $name");
-  return f.then((_)=>print("PASSED $name"),onError: (e){print("ERROR $name => $e"); throw(e);});
+void main() {
+  setUpAll(() => generate_connect().then((cmd) => cmd.send_object("FLUSHALL")));
+
 }
-
-main(){
-  Queue<Future> q =new Queue();
-  q.add(testing_helper(test_transactions(10000), "transaction"));
-  q.add(testing_helper(test_incr_fakecas(),"transaction FAKECAS"));
-  q.add(testing_helper(test_incr_fakecas_multiple(10),"transation FAKECAS multiple"));
-  q.add(testing_helper(test_unicode(), "unicode"));
-  q.add(testing_helper(test_transactions_failing(),"transation error handling")); 
-  q.add(testing_helper(test_transactions_command_usable(),"transaction release connection"));
-  q.add(testing_helper(test_pubsub(),"pubsub"));
-  q.add(testing_helper(test_commands_one_by_one(),"one by one")); 
-  q.add(testing_helper(testincrcasmultiple(),"CAS"));
-  q.add(testing_helper(testluanative(),"eval"));
-  q.add(testing_helper(testError(),"throw RedisError instead of return"));
-  q.add(testing_helper(testConversion(),"Convert ints to strings when in Array"));
-
-  Future.wait(q)
-  .then((_){
-    return testing_performance(test_muliconnections_con(100),"100 connections",200000);
-  })
-  .then((_)=>testing_performance(test_pubsub_performance,"pubsub performance",20000))
-  .then((_){
-    //just increase this number if you have more time (I did, but I lost paitence)
-    return testing_helper(test_long_running(20000),"one by one for longer time");
-  })
-  .then((_){
-    return testing_performance(test_performance,"raw performance",200000);
-  })
-  .then((_){
-    print("all tests PASSED!");
-    exit(0);
-  })
-  .catchError((e){
-    print("some of tests FAILED! $e");
-    exit(-1);
-  });
-}
-
