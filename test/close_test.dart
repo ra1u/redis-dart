2c3749a527c1ee59f7baae097c4cb833a0b4a3ca
Luka Rahne <luka.rahne@gmail.com>
2020-04-19 10:27:26 +0200

    update test for unexpected close

--- test/close_test.dart
+++ test/close_test.dart
@@ -8,17 +8,25 @@ main() {
     test("Expect error when sending Garbage", () async {
       Command cmd = await generate_connect();
       await cmd.send_object(["SET", "test", "0"]);
+      var last_future;
+      bool p = true;
       for (int i = 1; i <= 100000; i++) {
-        cmd.send_object(["INCR", "test"]).then((v) {
+        last_future = cmd.send_object(["INCR", "test"]).then((v) {
           if (i != v) {
             throw ("wrong received value, we got $v");
           }
-        }).catchError((e) {
+      }).catchError((e) {
+          if(p){
+            //Type t = typeof(e);
+            print("error $e ${e.runtimeType}");
+            p = false;
+          }
           // stream closed
         });
       }
       await cmd.get_connection().close();
-      //expect(cmd.send_object("GARBAGE"), throwsA(isRedisError));
+      await last_future;
+      //expect(cmd.send_object("GARBAGE"), throwsA(RedisError));
     });
   });
 }
