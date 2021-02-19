d8fa9aca57538b208b2a548561ba0ef233f3b1a7
Derrick Liu <derrick56007@hotmail.com>
2021-02-19 07:17:24 +0000

    enable secure connection

--- lib/connection.dart
+++ lib/connection.dart
@@ -7,29 +7,37 @@
  * Luka Rahne
  */
 
-
 part of redis;
 
 /// Class for server connection on server
-class RedisConnection{
+class RedisConnection {
   Socket _socket = null;
   LazyStream _stream = null;
   Future _future = new Future.value();
   RedisParser parser = new RedisParser();
-  
+
   /// connect on Redis server as client
-  Future<Command> connect(host, port){
-    return Socket.connect(host, port)
-    .then((Socket sock){
+  Future<Command> connect(host, port) {
+    return Socket.connect(host, port).then((Socket sock) {
       _socket = sock;
       disable_nagle(true);
-      _stream =new LazyStream.fromstream(_socket);
+      _stream = new LazyStream.fromstream(_socket);
       return new Command(this);
     });
   }
-  
+
+  /// connect on Redis server as client
+  Future<Command> connectSecure(host, port) {
+    return SecureSocket.connect(host, port).then((SecureSocket sock) {
+      _socket = sock;
+      disable_nagle(true);
+      _stream = new LazyStream.fromstream(_socket);
+      return new Command(this);
+    });
+  }
+
   /// close connection to Redis server
-  Future close(){
+  Future close() {
     _stream.close();
     return _socket.close();
   }
@@ -37,28 +45,28 @@ class RedisConnection{
   //this doesnt send anything
   //it just wait something to come from socket
   //it parse it and execute future
-  Future _senddummy(){
+  Future _senddummy() {
     _future = _future.then((_) {
-        return RedisParser.parseredisresponse(_stream);
+      return RedisParser.parseredisresponse(_stream);
     });
     return _future;
   }
-  
+
   // return future that complets
   // when all prevous _future finished
-  Future _getdummy(){
-    _future = _future.then((_){
-	    return "dummy data";
+  Future _getdummy() {
+    _future = _future.then((_) {
+      return "dummy data";
     });
     return _future;
   }
-  
-  Future _sendraw(List data){
+
+  Future _sendraw(List data) {
     _socket.add(data);
     return _senddummy();
   }
-  
-  void disable_nagle(bool v){
-    _socket.setOption(SocketOption.TCP_NODELAY,v);
+
+  void disable_nagle(bool v) {
+    _socket.setOption(SocketOption.TCP_NODELAY, v);
   }
 }
