adc699e3e44f2071866f755b7bdf0ab4312473d5
Luka Rahne <luka.rahne@gmail.com>
2021-07-02 17:04:10 +0200

    add podman suport for running local tests

--- test/docker/Readme.txt
+++ test/docker/Readme.txt
@@ -10,3 +10,10 @@ to run this test execute
 docker-compose up --abort-on-container-exit && echo ok
 
 In case of funny errors remove pubspec.lock in top dir
+
+Extra:
+
+We also provide script for podman that can be used for rootless execution.
+In that case docker is not needed, podman only.
+
+./run_podman.sh
---
+++ test/docker/run_podman.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# run tests localy with podman instead of docker
+# can be executed rootless
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+POD=$(podman pod create)
+
+echo "POD ${POD}"
+
+# cleanup on script exit
+trap "podman pod rm -f ${POD}" EXIT
+
+# run redis in pod
+REDIS_IMAGE=redis
+podman run \
+	   --detach \
+	   --rm \
+	   --pod ${POD} \
+	   ${REDIS_IMAGE}
+
+# run dart in pod
+DART_IMAGE=google/dart:2.12
+podman run \
+	   --rm \
+	   --pod ${POD} \
+	   --entrypoint "/bin/sh" \
+	   --env REDIS_URL=127.0.0.1 \
+	   --env REDIS_PORT=6379 \
+	   --volume "${SCRIPT_DIR}/../../:/workdir" \
+	   ${DART_IMAGE} \
+	   -c "set -e 
+           sleep 1
+           cd /workdir
+           dart --version
+           dart pub get
+           dart analyze || true
+           dart test
+           dart test/performance.dart"
+
