e4bd449a9609ca5bc578d1a4d6e68da719865f16
Luka Rahne <luka.rahne@gmail.com>
2026-03-16 22:18:18 +0100

    fix pub_sub tests

--- test/pubsub_test.dart
+++ test/pubsub_test.dart
@@ -1,61 +1,90 @@
 import 'package:redis/redis.dart';
 import 'package:test/test.dart';
+import 'package:async/async.dart';
 
 import 'main.dart';
 
 void main() async {
-  Command cmdP = await generate_connect();
-  Command cmdS = await generate_connect();
+
 
   group("Test Redis Pub-Sub", () {
-    PubSub subscriber = PubSub(cmdS);
 
-    test("Publishing to channel before subscription", () {
-      expect(cmdP.send_object(["PUBLISH", "test", "hello"]),
+
+    test("Publishing to channel before subscription", () async {
+      Command cmdP = await generate_connect();
+      Command cmdS = await generate_connect();
+      PubSub subscriber = PubSub(cmdS);
+
+      String chan = "test_pub_before_sub";
+      expect(cmdP.send_object(["PUBLISH", chan, "hello"]),
           completion(equals(0)));
     });
 
-    test("Subscribe to channel", () {
-      expect(() => subscriber.subscribe(["test"]), returnsNormally,
+    test("Subscribe to channel", () async {
+      Command cmdP = await generate_connect();
+      Command cmdS = await generate_connect();
+      PubSub subscriber = PubSub(cmdS);
+      
+      String chan = "test_sub";
+      expect(() => subscriber.subscribe([chan]), returnsNormally,
           reason: "No error should be thrown when subscribing to channel.");
 
-      expect(cmdP.send_object(["PUBSUB", "NUMSUB", "test"]),
-          completion(equals(["test", 1])),
+      var queue = StreamQueue(subscriber.getStream());
+      expect(await queue.next, equals(["subscribe", chan, 1]));
+      
+      expect(cmdP.send_object(["PUBSUB", "NUMSUB", chan]),
+          completion(equals([chan, 1])),
           reason: "Number of subscribers should be 1 after subscription");
 
+      
       expect(
           () => cmdS.send_object("PING"),
           throwsA(equals("PubSub on this connaction in progress"
               "It is not allowed to issue commands trough this handler")),
           reason: "After subscription, command should not be able to send");
+      
     });
 
-    test("Publishing to channel", () {
-      expect(cmdP.send_object(["PUBLISH", "test", "goodbye"]),
+    test("Publishing to channel", () async {
+      Command cmdP = await generate_connect();
+      Command cmdS = await generate_connect();
+      PubSub subscriber = PubSub(cmdS);
+
+      String chan = "test_pub";
+
+      expect(() => subscriber.subscribe([chan]), returnsNormally,
+          reason: "No error should be thrown when subscribing to channel.");
+
+      var queue = StreamQueue(subscriber.getStream());
+      expect(await queue.next, equals(["subscribe", chan, 1]));
+
+      expect( cmdP.send_object(["PUBLISH", chan, "goodbye"]),
           completion(equals(1)));
+      
+      expect(await queue.next, equals(["message", chan, "goodbye"]));
+    });
+
+    test("Unsubscribe channel", () async {
+      Command cmdP = await generate_connect();
+      Command cmdS = await generate_connect();
+      PubSub subscriber = PubSub(cmdS);
+
+      String chan = "test_unsub";
+      expect(() => subscriber.unsubscribe([chan]), returnsNormally,
+          reason: "No error should be thrown when subscribing to channel.");
 
       expect(
           subscriber.getStream(),
           emitsInOrder([
-            ["subscribe", "test", 1],
-            ["message", "test", "goodbye"]
+            ["unsubscribe", chan, 0],
           ]),
           reason: "After subscribing, the message should be received.");
-    });
 
-    test("Unsubscribe channel", () async {
-      expect(() => subscriber.unsubscribe(["test"]), returnsNormally,
-          reason: "No error should be thrown when subscribing to channel.");
-
-      // Wait for Redis to confirm the unsubscribe before asserting
-      await subscriber.getStream().firstWhere(
-          (msg) => msg[0] == "unsubscribe" && msg[1] == "test");
-
-      expect(cmdP.send_object(["PUBSUB", "NUMSUB", "test"]),
-          completion(equals(["test", 0])),
+      expect(cmdP.send_object(["PUBSUB", "NUMSUB", chan]),
+          completion(equals([chan, 0])),
           reason: "Number of subscribers should be 0 after unsubscribe");
 
-      expect(cmdP.send_object(["PUBLISH", "test", "goodbye"]),
+      expect(cmdP.send_object(["PUBLISH", chan, "goodbye"]),
           completion(equals(0)),
           reason:
               "Publishing a message after unsubscribe should be received by zero clients.");
@@ -64,6 +93,9 @@ void main() async {
     });
 
     test("Test close", () async {
+      Command cmdP = await generate_connect();
+      Command cmdS = await generate_connect();
+      PubSub subscriber = PubSub(cmdS);
       // test that we can close connection
       // creates new connection as prevously used in test
       // does not expect errors
